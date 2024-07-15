b7c306bb8a4ee5d6987fe3671237103683d07539
Didier Pinto <didieresnaki@hotmail.com>
2024-07-15 17:53:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ecomodelo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ecomodelo;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int id;
+    private String nombre;
+    private String email;
+    private String password;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
